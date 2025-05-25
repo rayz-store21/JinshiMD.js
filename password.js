7565a15eb5f51eec1856001230dd2b05d437e3f7
rayz-store21 <gediken04@gmail.com>
2025-05-25 16:34:48 +0700

    Create password.js

---
+++ password.js
@@ -0,0 +1,17 @@
+{
+
+  "secretCode": "RayzGanteng",
+
+  "description": "Jinshi New Meta", 
+
+  "lastUpdated": "14/05/2025",
+
+  "author": "RayzPemula", 
+
+  "status": "Active",
+
+  "license": "MIT",
+
+  "version": "6.0"
+
+}
